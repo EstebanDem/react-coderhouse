da5c64efebd429f14bf51c54aa538cd8e0ed5663
Esteban Demarchi <estebanjosedemarchi@gmail.com>
2021-12-20 20:28:04 -0300

    :heavy_plus_sign: Added router-react-dom

--- src/mock/MockedItems.jsx
+++ src/mock/MockedItems.jsx
@@ -2,84 +2,84 @@
 
 const MockedItems = [
     {
-        id: 1,
+        id: "1",
         title: 'Producto 1',
         description: 'Some dummy description 1',
         price: 500,
         pictureUrl: '/images/imgDefault.png'
     },
     {
-        id: 2,
+        id: "2",
         title: 'Producto 2',
         description: 'Some dummy description 2',
         price: 1500,
         pictureUrl: '/images/imgDefault.png'
     },
     {
-        id: 3,
+        id: "3",
         title: 'Producto 3',
         description: 'Some dummy description 3',
         price: 550,
         pictureUrl: '/images/imgDefault.png'
     },
     {
-        id: 4,
+        id: "4",
         title: 'Producto 4',
         description: 'Some dummy description 4',
         price: 550,
         pictureUrl: '/images/imgDefault.png'
     },
     {
-        id: 5,
+        id: "5",
         title: 'Producto 5',
         description: 'Some dummy description 5',
         price: 550,
         pictureUrl: '/images/imgDefault.png'
     },
     {
-        id: 6,
+        id: "6",
         title: 'Producto 6',
         description: 'Some dummy description 6',
         price: 550,
         pictureUrl: '/images/imgDefault.png'
     },
     {
-        id: 7,
+        id: "7",
         title: 'Producto 7',
         description: 'Some dummy description 7',
         price: 550,
         pictureUrl: '/images/imgDefault.png'
     },
     {
-        id: 8,
+        id: "8",
         title: 'Producto 8',
         description: 'Some dummy description 8',
         price: 550,
         pictureUrl: '/images/imgDefault.png'
     },
     {
-        id: 9,
+        id: "9",
         title: 'Producto 9',
         description: 'Some dummy description 9',
         price: 550,
         pictureUrl: '/images/imgDefault.png'
     },
     {
-        id: 10,
+        id: "10",
         title: 'Producto 10',
         description: 'Some dummy description 10',
         price: 550,
         pictureUrl: '/images/imgDefault.png'
     },
     {
-        id: 11,
+        id: "11",
         title: 'Producto 11',
         description: 'Some dummy description 11',
         price: 550,
         pictureUrl: '/images/imgDefault.png'
     },
     {
-        id: 12,
+        id: "12",
         title: 'Producto 12',
         description: 'Some dummy description 12',
         price: 550,
