e4d11cccf07840c097230c07fb9c2ea0473bbe75
Esteban Demarchi <estebanjosedemarchi@gmail.com>
2021-12-20 21:04:48 -0300

    :bento: Added pictures

--- src/mock/MockedItems.jsx
+++ src/mock/MockedItems.jsx
@@ -3,87 +3,135 @@
 const MockedItems = [
     {
         id: "1",
-        title: 'Producto 1',
+        title: 'Are you experienced?',
+        artist: '',
+        category: 'Rock Psicodélico',
+        expertsChoice: true,
         description: 'Some dummy description 1',
+        longDescription: 'asdklaskdlsaj dsakd saiojd osiajd opaskdopasd pasdl poád posakdp osakopd aspospdska po pok sa aopdkspdaspo skodaspdksa kopdkaskaoop',
         price: 500,
-        pictureUrl: '/images/imgDefault.png'
+        pictureUrl: '/images/covers/are_you_experienced.jpg'
     },
     {
         id: "2",
-        title: 'Producto 2',
+        title: 'One Size Fits All',
+        artist: '',
+        category: 'Rock Progresivo',
+        expertsChoice: false,
         description: 'Some dummy description 2',
+        longDescription: '',
         price: 1500,
-        pictureUrl: '/images/imgDefault.png'
+        pictureUrl: '/images/covers/one_size_fits_all.jpg'
     },
     {
         id: "3",
-        title: 'Producto 3',
+        title: 'In the court of the Crimson King',
+        artist: '',
+        category: '',
+        expertsChoice: false,
         description: 'Some dummy description 3',
+        longDescription: '',
         price: 550,
-        pictureUrl: '/images/imgDefault.png'
+        pictureUrl: '/images/covers/court.jpg'
     },
     {
         id: "4",
-        title: 'Producto 4',
+        title: 'Atom Heart Mother',
+        artist: 'Pink Floyd',
+        category: '',
+        expertsChoice: false,
         description: 'Some dummy description 4',
+        longDescription: '',
         price: 550,
-        pictureUrl: '/images/imgDefault.png'
+        pictureUrl: '/images/covers/Pink_Floyd_AHM.jpg'
     },
     {
         id: "5",
-        title: 'Producto 5',
+        title: 'Temple Of The Dog',
+        artist: 'Temple Of The Dog',
+        category: '',
+        expertsChoice: false,
         description: 'Some dummy description 5',
+        longDescription: '',
         price: 550,
-        pictureUrl: '/images/imgDefault.png'
+        pictureUrl: '/images/covers/TOTD.jpg'
     },
     {
         id: "6",
-        title: 'Producto 6',
+        title: 'Mad Season',
+        artist: 'Mad Season',
+        category: '',
+        expertsChoice: false,
         description: 'Some dummy description 6',
+        longDescription: '',
         price: 550,
-        pictureUrl: '/images/imgDefault.png'
+        pictureUrl: '/images/covers/mad_season.jpg'
     },
     {
         id: "7",
         title: 'Producto 7',
+        artist: '',
+        category: '',
+        expertsChoice: false,
         description: 'Some dummy description 7',
+        longDescription: '',
         price: 550,
-        pictureUrl: '/images/imgDefault.png'
+        pictureUrl: '/images/covers/aic.jpg'
     },
     {
         id: "8",
-        title: 'Producto 8',
+        title: '10.000 Days',
+        artist: 'TOOL',
+        category: '',
+        expertsChoice: false,
         description: 'Some dummy description 8',
+        longDescription: '',
         price: 550,
-        pictureUrl: '/images/imgDefault.png'
+        pictureUrl: '/images/covers/tool.jpg'
     },
     {
         id: "9",
-        title: 'Producto 9',
+        title: 'Close to the edge',
+        artist: 'Yes',
+        category: '',
+        expertsChoice: false,
         description: 'Some dummy description 9',
+        longDescription: '',
         price: 550,
-        pictureUrl: '/images/imgDefault.png'
+        pictureUrl: '/images/covers/yes.jpg'
     },
     {
         id: "10",
-        title: 'Producto 10',
+        title: 'Obscured by clouds',
+        artist: 'Pink Floyd',
+        category: '',
+        expertsChoice: false,
         description: 'Some dummy description 10',
+        longDescription: '',
         price: 550,
-        pictureUrl: '/images/imgDefault.png'
+        pictureUrl: '/images/covers/obscured.jpg'
     },
     {
         id: "11",
         title: 'Producto 11',
+        artist: '',
+        category: '',
+        expertsChoice: false,
         description: 'Some dummy description 11',
+        longDescription: '',
         price: 550,
-        pictureUrl: '/images/imgDefault.png'
+        pictureUrl: '/images/covers/velvet.jpg'
     },
     {
         id: "12",
         title: 'Producto 12',
+        artist: '',
+        category: '',
+        expertsChoice: false,
         description: 'Some dummy description 12',
+        longDescription: '',
         price: 550,
-        pictureUrl: '/images/imgDefault.png'
+        pictureUrl: '/images/covers/electric.jpg'
     },
 ];
 
